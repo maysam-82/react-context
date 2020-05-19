cba987ee41d5f0c6bcf51e42d06d5f9c68e821ed
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-05-19 23:21:10 +1000

    implement Provide

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import UserCreate from './UserCreate';
+import LanguageContext from '../context/LanguageContext';
+
 class App extends Component {
 	state = {
 		language: 'english',
@@ -23,7 +25,12 @@ class App extends Component {
 						onClick={() => this.onLanguageChanged('persian')}
 					/>
 				</div>
-				<UserCreate />
+				{/* Provider is used to connect App component to context object. */}
+				{/* Component that needs information from context object */}
+				{/* value props is an special prop of Provider which contain value that we are going to put inside of context object which here is `state.language`*/}
+				<LanguageContext.Provider value={this.state.language}>
+					<UserCreate />
+				</LanguageContext.Provider>
 			</div>
 		);
 	}
