bce92be0298c902488a2b542cd365a7064553d55
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-05-19 19:02:10 +1000

    connect context object to components

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
+import LanguageContext from '../context/LanguageContext';
 
 export default class Button extends Component {
+	// contextType is going to link Button to context object. We connected a context object to Button component. `contextType` must not be renamed.
+	// `static` adds a property directly to a class and it is equivalent to `Botton.contextType = LanguageContext` after creating a class.
+	static contextType = LanguageContext;
+
+	// After creating contextType, we can reference this.context and access data inside context object.
 	render() {
-		return <button className="ui button primary">Submit</button>;
+		const buttonTitle = this.context === 'english' ? 'Submit' : 'ارسال';
+		return <button className="ui button primary">{buttonTitle}</button>;
 	}
 }
--- src/components/Field.js
+++ src/components/Field.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import LanguageContext from '../context/LanguageContext';
 
 export default class Field extends Component {
+	static contextType = LanguageContext;
 	render() {
+		const labelName = this.context === 'english' ? 'Name' : 'نام';
 		return (
 			<div className="ui field">
-				<label htmlFor="name">Name</label>
+				<label htmlFor="name">{labelName}</label>
 				<input type="text" name="name" id="name" />
 			</div>
 		);
